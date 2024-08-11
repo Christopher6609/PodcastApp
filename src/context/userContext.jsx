9fba4780430d9902943583c597773205b55ecafd
Christopher <odeniyichristopher7@gmail.com>
2024-08-11 23:51:49 +0100

    migrated userContext into reducer

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,38 +1,65 @@
-import {createContext, useState, useEffect } from 'react';
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from '../utils/firebase/firebase';
-import PropTypes from 'prop-types';
-
+import { createContext, useEffect, useReducer } from "react";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "../utils/firebase/firebase";
+import PropTypes from "prop-types";
 
 export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => {}
-})
+  currentUser: null,
+  
+});
 
-export const UserProvider = ({children}) => {
+const INITIAL_STATE = {
+  currentUser: null,
+};
 
-    const [ currentUser, setCurrentUser ] = useState({});
-    const value = { currentUser, setCurrentUser };
-    
+const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
 
-    // this function listens to whenever authentication changes and either creates a new user document in the database or set the current user to the user 
-    useEffect(()=>{
-        const unsubscribeUser = onAuthStateChangedListener((user)=>{
-            if(user){
-                createUserDocumentFromAuth(user);
-            }
-            setCurrentUser(user);
-        });
-       
-        return unsubscribeUser;
-    },[])
+const userReducer = (state, action) => {
+  const { type, payload } = action;
 
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`unhandled type ${type} at user reducer`);
+  }
+};
 
+export const UserProvider = ({ children }) => {
+  //const [currentUser, setCurrentUser] = useState({});
 
-    return(
-        <UserContext.Provider value={value}>{children}</UserContext.Provider>
-    )
-}
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+  const setCurrentUser =  (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    // await createUserDocumentFromAuth(user);
+    // setCurrentUser(user);
+  };
+
+  const value = { currentUser, setCurrentUser };
+
+  //this function listens to whenever authentication changes and either creates a new user document in the database or set the current user to the user
+    useEffect(() => {
+      const unsubscribeUser = onAuthStateChangedListener((user) => {
+        if (user) {
+          createUserDocumentFromAuth(user);
+        }
+        setCurrentUser(user);
+      });
+
+      return unsubscribeUser;
+    }, []);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
 
 UserProvider.propTypes = {
-    children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired,
+};
--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,92 +1,95 @@
 import { useState } from "react";
 import Button from "../../components/atoms/Button/Button";
 import FormInput from "../../components/atoms/FormInput/FormInput";
-import { signInWithGooglePopup, signUserInWithEmailAndPassword } from "../../utils/firebase/firebase";
+import {
+  signInWithGooglePopup,
+  signUserInWithEmailAndPassword,
+} from "../../utils/firebase/firebase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
-
 const SignIn = () => {
+  const DefaultFormFields = {
+    email: "",
+    password: "",
+  };
 
-    const DefaultFormFields = {
-        email:'',
-        password:''
+  const [formFields, setFormFields] = useState(DefaultFormFields);
+  const { email, password } = formFields;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const resetFormFields = () => setFormFields(DefaultFormFields);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await signUserInWithEmailAndPassword(email, password);
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/invalid-credential":
+          alert("Invalid Login Credentials");
+          break;
+        case "auth/user-not-found":
+          alert("User not found");
+          break;
+        default:
+          console.log(error);
+      }
+      console.log(error);
     }
+  };
 
-    const [formFields, setFormFields] = useState(DefaultFormFields);
-    const {email, password } = formFields;
+  
 
-    const handleChange = (event) => {
-        const {name, value } = event.target;
-        setFormFields({...formFields, [name]:value})
-    }
-    
-    const resetFormFields = () => setFormFields(DefaultFormFields);
+  const googleSignIn = async () => {
+    await signInWithGooglePopup();
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        try{
-            await signUserInWithEmailAndPassword(email,password);
-            resetFormFields();
-        }catch(error){
-            switch(error.code){
-                case 'auth/invalid-credential' :
-                    alert('Invalid Login Credentials');
-                    break;
-                case 'auth/user-not-found':
-                    alert('User not found');
-                    break;
-                default:
-                    console.log(error);
-            }
-            console.log(error);
-        }
-    }
-
-    
-
-    
-
-    const googleSignIn = async () => {
-      await signInWithGooglePopup();
-     
-    };
-
-    return(
-        <>
-             <div className="flex flex-col justify-center items-center mt-[3rem]">
-                    <h2>I have an account</h2>
-                    <h1 className='text-[2rem]'>Login with your Email and Password</h1>
-            <div className="w-[50rem] flex flex-col gap-[2rem]">
-                <form onSubmit={handleSubmit} className="">
-                    <FormInput 
-                        type="email"
-                        placeholder="Enter your email"
-                        name="email"
-                        value={email}
-                        onChange={handleChange}
-                        required
-                    />
-                    <FormInput 
-                        type="password"
-                        placeholder="Enter your password"
-                        name="password"
-                        value={password}
-                        onChange={handleChange}
-                        required
-                    />
-                    <div className="mt-[2rem] flex gap-[2rem]">
-                        <span><Button>Log in</Button></span>
-                        <span onClick={googleSignIn}><Button ><FontAwesomeIcon icon={faGoogle} className="pr-[0.875rem]"/>Log in with Google</Button></span>
-                    </div>
-                    
-                </form>
-               
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center mt-[3rem]">
+        <h2>I have an account</h2>
+        <h1 className="text-[2rem]">Login with your Email and Password</h1>
+        <div className="w-[50rem] flex flex-col gap-[2rem]">
+          <form onSubmit={handleSubmit} className="">
+            <FormInput
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              required
+            />
+            <FormInput
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+            <div className="mt-[2rem] flex gap-[2rem]">
+              <span>
+                <Button>Log in</Button>
+              </span>
+              <span onClick={googleSignIn}>
+                <Button>
+                  <FontAwesomeIcon icon={faGoogle} className="pr-[0.875rem]" />
+                  Log in with Google
+                </Button>
+              </span>
             </div>
-            </div>
-        </>
-    )
-}
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SignIn;
